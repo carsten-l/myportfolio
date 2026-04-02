290e463d286464592fde7ca494a9a427432aa569
clurts <clu@rts.dk>
2026-04-02 14:10:57 +0200

    refactor homepage content: remove quote and link, streamline text for clarity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import Link from "next/link";
-import { FaArrowRightLong } from "react-icons/fa6";
 import Image from "next/image";
 import profileimg from "./assets/profile-img.jpeg";
 
@@ -22,27 +20,18 @@ export default function Home() {
         creating visually appealing user experiences with the modern JavaScript
         tech stack.
       </p>
-
-      <blockquote cite="https://en.wikiquote.org/wiki/Linus_Torvalds">
-        <p className="font-mono text-lg">Talk is cheap — show me the code.</p>
-        <footer className="text-sm font-mono italic mt-1">
-          — <cite>Linus Torvalds</cite>
-        </footer>
-      </blockquote>
-
-      <p className="text-xl leading-8">
-        Well, here it is. The code. On this site I showcase some of my projects.
+      <p className="text-xl leading-relaxed md:w-4/5 xl:w-3/5">
+        But don&apos;t tell it &ndash; show it! Right...
       </p>
 
-      <div className="flex gap-4 items-center flex-col sm:flex-row">
-        <Link
-          className="rounded-full tracking-wide border-2 border-accent
-           flex items-center justify-center gap-4 hover:bg-accent hover:text-white  text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-          href="my-projects"
-        >
-          Have a look around <FaArrowRightLong />
-        </Link>
-      </div>
+      <p className="text-xl leading-relaxed md:w-4/5 xl:w-3/5">
+        On this site I tell you about the frameworks and tools I enjoy using,
+        and I showcase some of my projects.
+      </p>
+
+      <p className="text-xl leading-relaxed md:w-4/5 xl:w-3/5">
+        Have a look around...
+      </p>
     </main>
   );
 }
